58364bef7a59e3ab1fab4d53db574eb90fe75b3d
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-08-23 18:01:56 +0100

    root url is working

--- src/App.py
+++ src/App.py
@@ -4,6 +4,10 @@ import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Welcome to my Flask-React app!"
+
 @app.route('/search-tracks', methods=['GET'])
 def search_tracks():
     search_term = request.args.get('q')
@@ -26,7 +30,45 @@ def search_tracks():
         print('Error fetching from Spotify API:', e)
         return jsonify(error='Internal Server Error'), 500
 
-# Other routes for get-track-details and get-track-audio-features
+@app.route('/get-track-details/<track_id>', methods=['GET'])
+def get_track_details(track_id):
+    api_key = os.getenv('CLIENT_ID')
+    
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+    
+    try:
+        response = requests.get(
+            f'https://api.spotify.com/v1/tracks/{track_id}',
+            headers=headers
+        )
+        response.raise_for_status()  # Raise an error for non-2xx responses
+        track_details = response.json()
+        return jsonify(track_details)
+    except requests.exceptions.RequestException as e:
+        print('Error fetching from Spotify API:', e)
+        return jsonify(error='Internal Server Error'), 500
+
+@app.route('/get-track-audio-features/<track_id>', methods=['GET'])
+def get_track_audio_features(track_id):
+    api_key = os.getenv('CLIENT_ID')
+    
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+    
+    try:
+        response = requests.get(
+            f'https://api.spotify.com/v1/audio-features/{track_id}',
+            headers=headers
+        )
+        response.raise_for_status()  # Raise an error for non-2xx responses
+        audio_features = response.json()
+        return jsonify(audio_features)
+    except requests.exceptions.RequestException as e:
+        print('Error fetching from Spotify API:', e)
+        return jsonify(error='Internal Server Error'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
