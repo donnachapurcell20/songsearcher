fc3f421724c38218a029e4f8a7067749533be239
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-08-24 22:39:54 +0100

    Added some error handling and validation, also added more endpoints and changed few things around to App.py. SearchResults.js completely changed to allow for user to input either the artistname or the song name

--- src/App.py
+++ src/App.py
@@ -1,82 +1,112 @@
 from flask import Flask, jsonify, render_template, request
 import os
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 # Define your Spotify API credentials
-CLIENT_ID = os.getenv('Client_ID')
-CLIENT_SECRET = os.getenv('Client_secret')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+# Token management variables
+access_token = None
+expiration_time = datetime.min
+
+# Spotify API authorization helper function
+def get_spotify_access_token():
+    global access_token, expiration_time
+    
+    if expiration_time <= datetime.now():
+        auth_response = requests.post('https://accounts.spotify.com/api/token', data={
+            'grant_type': 'client_credentials',
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+        })
+
+        if auth_response.status_code != 200:
+            print("Failed to obtain access token from Spotify API")
+            print("Error response:", auth_response.text)  # Add this line to print the error response
+            raise Exception("Failed to obtain access token from Spotify API")
+
+        auth_data = auth_response.json()
+        access_token = auth_data['access_token']
+        expiration_seconds = auth_data['expires_in']
+        expiration_time = datetime.now() + timedelta(seconds=expiration_seconds)
+
+    return access_token
 
 # Endpoint for serving your React app
 @app.route('/')
 def serve_react_app():
     return render_template('index.html')
 
-# Spotify API authorization helper function
-def get_spotify_access_token():
-    auth_response = requests.post('https://accounts.spotify.com/api/token', data={
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-    })
-
-    auth_data = auth_response.json()
-    access_token = auth_data['access_token']
-    return access_token
-
 # API route for searching tracks on Spotify
 @app.route('/api/search-tracks')
 def search_tracks():
     search_term = request.args.get('q')
-    access_token = get_spotify_access_token()
+    if not search_term:
+        return jsonify(error='Missing search term'), 400
 
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
+    try:
+        access_token = get_spotify_access_token()
 
-    response = requests.get(
-        f'https://api.spotify.com/v1/search?q={search_term}&type=track',
-        headers=headers
-    )
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
 
-    response_data = response.json()
-    tracks = response_data.get('tracks', {}).get('items', [])
-    return jsonify(tracks)
+        response = requests.get(
+            f'https://api.spotify.com/v1/search?q={search_term}&type=track',
+            headers=headers
+        )
+
+        response_data = response.json()
+        tracks = response_data.get('tracks', {}).get('items', [])
+        return jsonify(tracks)
+    except Exception as e:
+        return jsonify(error='Error fetching from Spotify API'), 500
 
 # API route for getting track details from Spotify
 @app.route('/api/get-track-details/<track_id>')
 def get_track_details(track_id):
-    access_token = get_spotify_access_token()
+    try:
+        access_token = get_spotify_access_token()
 
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
 
-    response = requests.get(
-        f'https://api.spotify.com/v1/tracks/{track_id}',
-        headers=headers
-    )
+        response = requests.get(
+            f'https://api.spotify.com/v1/tracks/{track_id}',
+            headers=headers
+        )
 
-    track_details = response.json()
-    return jsonify(track_details)
+        track_details = response.json()
+        return jsonify(track_details)
+    except Exception as e:
+        return jsonify(error='Error fetching track details'), 500
 
 # API route for getting track audio features from Spotify
 @app.route('/api/get-track-audio-features/<track_id>')
 def get_track_audio_features(track_id):
-    access_token = get_spotify_access_token()
+    try:
+        access_token = get_spotify_access_token()
 
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
 
-    response = requests.get(
-        f'https://api.spotify.com/v1/audio-features/{track_id}',
-        headers=headers
-    )
+        response = requests.get(
+            f'https://api.spotify.com/v1/audio-features/{track_id}',
+            headers=headers
+        )
 
-    audio_features = response.json()
-    return jsonify(audio_features)
+        audio_features = response.json()
+        return jsonify(audio_features)
+    except Exception as e:
+        return jsonify(error='Error fetching audio features'), 500
 
 if __name__ == '__main__':
+    # Initialize access token
+    access_token = get_spotify_access_token()
     app.run(debug=True)
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,13 +1,42 @@
 // src/SearchResults.js
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function SearchResults({ results }) {
+function SearchResults({ artistName, songName }) {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    // Define your Spotify API credentials or access token here
+    const API_URL = 'https://api.spotify.com/v1/search';
+    const ACCESS_TOKEN = 'your_access_token_here';
+
+    // Construct the API request URL
+    const searchQuery = `${artistName} ${songName}`;
+    const encodedQuery = encodeURIComponent(searchQuery);
+    const searchUrl = `${API_URL}?q=${encodedQuery}&type=track`;
+
+    // Make the API request
+    fetch(searchUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        const tracks = data.tracks.items;
+        setResults(tracks);
+      })
+      .catch(error => {
+        console.error('Error fetching search results:', error);
+      });
+  }, [artistName, songName]);
+
   return (
     <div className="search-results">
       {results.map((result, index) => (
         <div key={index} className="result-item">
-          <h3>{result.title}</h3>
+          <h3>{result.name}</h3>
           {/* Display additional result information */}
         </div>
       ))}
