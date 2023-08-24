4db4c432473b8aba8a056c02cb4a7f14e664ce80
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-08-24 12:18:21 +0100

    Modified app,js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
-import React, { useState } from 'react';
-import Header from './components/Header'; // Adjust the path based on your project structure
-import InputForm from './components/InputForm'; // Adjust the path based on your project structure
-import SearchResults from './components/SearchResults'; // Adjust the path based on your project structure
-import axios from 'axios'; // Import axios
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
+    const [data, setData] = useState([]);
 
-  const handleSearch = async (searchTerm) => {
-    try {
-      const response = await axios.get(
-        `https://api.spotify.com/v1/search?q=${searchTerm}&type=track`,
-        {
-          headers: {
-            Authorization: 'b35ab11f052a491c9a2b4a95d8d60c9f',
-          },
-        }
-      );
+    useEffect(() => {
+        // Make API call to Flask backend
+        axios.get('/api/search-tracks')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
 
-      const tracks = response.data.tracks.items;
-      setSearchResults(tracks);
-    } catch (error) {
-      console.error('Error fetching from Spotify API:', error);
-    }
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      <main className="container">
-        <InputForm onSearch={handleSearch} />
-        <SearchResults results={searchResults} />
-      </main>
-    </div>
-  );
+    // Render your React components
+    return (
+        <div>
+            {/* Render your components using the data */}
+        </div>
+    );
 }
 
 export default App;
