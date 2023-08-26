d4cb74cdbf0a0149b10c8b3b2dc8bb316d6685c4
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-08-26 15:22:24 +0100

    Made changes to package.json and App.js so they work together

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import InputForm from './components/InputForm';
 import SearchResults from './components/SearchResults';
-import Footer from './components/Footer'; // Import the Footer component
+import Footer from './components/Footer';
 
 function App() {
   const [searchParams, setSearchParams] = useState({
@@ -28,10 +28,12 @@ function App() {
           />
           <Route
             path="/search-tracks"
-            element={<SearchResults
-                        artistName={searchParams.artistName}
-                        songName={searchParams.songName}
-                      />}
+            element={
+              <SearchResults
+                artistName={searchParams.artistName}
+                songName={searchParams.songName}
+              />
+            }
           />
           {/* Add more routes/components as needed */}
         </Routes>
