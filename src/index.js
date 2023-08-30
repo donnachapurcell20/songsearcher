ee2277b0d31eee74a4fb1cb41ef72b0513ccc422
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-08-30 15:58:09 +0100

    Added import for reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 // Initial render
 root.render(<App />);
-
-// Later, you can update the app's content without completely recreating the root
-// root.render(<UpdatedApp />);
 
 reportWebVitals();
